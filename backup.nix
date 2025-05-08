61322d4f9e0241895ffe089bc0a42b41c991c676
Ivan Filipenkov <hello@vanutp.dev>
2025-05-08 04:04:39 +0200

    try to fix backup

--- backup.nix
+++ backup.nix
@@ -4,31 +4,33 @@
   ...
 }: {
   systemd.user.services.backup-hse-modded = {
-    Service.ExecStart = lib.getExe (
-      pkgs.writeShellApplication {
-        name = "backup-hse-modded";
+    Service = {
+      Type = "oneshot";
+      ExecStart = lib.getExe (
+        pkgs.writeShellApplication {
+          name = "backup-hse-modded";
 
-        runtimeInputs = with pkgs; [
-          coreutils
-          rdiff-backup
-        ];
+          runtimeInputs = with pkgs; [
+            coreutils
+            rdiff-backup
+          ];
 
-        text = ''
-          mkdir -p ~/backup
-          rdiff-backup backup \
-            ~/hse-modded ~/backup
-          rdiff-backup --force remove increments \
-            --older-than 7D ~/backup || true
-        '';
-      }
-    );
+          text = ''
+            mkdir -p ~/backup
+            rdiff-backup backup \
+              ~/hse-modded ~/backup
+            rdiff-backup --force remove increments \
+              --older-than 7D ~/backup || true
+          '';
+        }
+      );
+    };
   };
 
   systemd.user.timers.backup-hse-modded = {
     Timer = {
       OnCalendar = "*-*-* *:00:00";
-      Unit = "backup-hse-modded.service";
     };
-    Install.WantedBy = ["default.target"];
+    Install.WantedBy = ["timers.target"];
   };
 }
